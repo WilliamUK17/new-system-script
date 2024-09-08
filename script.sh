e6b7f1cc461f0e9f248a303a803f0070b7c0377d
William Davies <williamfdavies123@gmail.com>
2024-09-08 13:16:10 +0100

    updates

--- script.sh
+++ script.sh
@@ -27,9 +27,9 @@ fi
 
 # Description of what the script does
 print_info "This script will:"
-echo "1. Add one or more new users with the specified usernames."
+echo "1. Add a new user with the specified username."
 echo "2. Optionally copy the .ssh folder to the new user's home directory and set correct permissions."
-echo "3. Optionally add the users to the sudo group."
+echo "3. Optionally add the user to the sudo group."
 echo "4. Optionally install Docker and Docker Compose."
 
 # Ask the user if they want to continue
@@ -39,53 +39,38 @@ if [[ "$confirm" != "y" ]]; then
   exit 0
 fi
 
-# Function to add a user
-add_new_user() {
-  local username
-  read -p "Enter the username you want to create: " username
-
-  print_info "Adding user '$username'"
-  adduser "$username"
-
-  # Ask if the user wants to copy the .ssh folder
-  read -p "Do you want to copy the current .ssh folder to the new user's home directory? (y/n): " copy_ssh
-  if [[ "$copy_ssh" == "y" ]]; then
-    print_info "Copying .ssh folder to /home/$username/"
-    cp -r ~/.ssh /home/"$username"/
-    chown -R "$username":"$username" /home/"$username"/.ssh
-  else
-    print_info ".ssh folder will not be copied."
-  fi
-
-  # Ask if the user should be added to the sudo group
-  read -p "Do you want to add user '$username' to the sudo group? (y/n): " add_sudo
-  if [[ "$add_sudo" == "y" ]]; then
-    print_info "Adding user '$username' to the sudo group"
-    usermod -aG sudo "$username"
-  fi
-
-  # Add the new user to the docker group later if Docker is installed
-  echo "$username"
-}
-
-# Add at least one user, then ask if more users should be added
-usernames=()
-usernames+=("$(add_new_user)")
-
-while true; do
-  read -p "Do you want to add another user? (y/n): " add_another_user
-  if [[ "$add_another_user" == "y" ]]; then
-    usernames+=("$(add_new_user)")
-  else
-    break
-  fi
-done
-
-# Ask if the user wants to install Docker
+# Ask all the questions at the start
+read -p "Enter the username you want to create: " username
+read -p "Do you want to copy the current .ssh folder to the new user's home directory? (y/n): " copy_ssh
+read -p "Do you want to add user '$username' to the sudo group? (y/n): " add_sudo
 read -p "Do you want to install Docker? (y/n): " install_docker
-
 if [[ "$install_docker" == "y" ]]; then
-  # Ask the user for the operating system
+  read -p "Do you want to install Docker Compose as well? (y/n): " install_docker_compose
+fi
+
+# Add the user
+print_info "Adding user '$username' using adduser"
+adduser "$username"
+
+# Copy the .ssh folder if requested
+if [[ "$copy_ssh" == "y" ]]; then
+  print_info "Copying .ssh folder to /home/$username/"
+  cp -r ~/.ssh /home/"$username"/
+  chown -R "$username":"$username" /home/"$username"/.ssh
+else
+  print_info ".ssh folder will not be copied."
+fi
+
+# Add the user to the sudo group if requested
+if [[ "$add_sudo" == "y" ]]; then
+  print_info "Adding user '$username' to the sudo group"
+  usermod -aG sudo "$username"
+else
+  print_info "User '$username' will not be added to the sudo group."
+fi
+
+# Install Docker if requested
+if [[ "$install_docker" == "y" ]]; then
   echo -e "${YELLOW}Please select your operating system:${NC}"
   echo "1. Debian 11 & 12"
   echo "2. Ubuntu 20.04 (Focal)"
@@ -95,37 +80,45 @@ if [[ "$install_docker" == "y" ]]; then
   echo "6. CentOS 7"
   read -p "Enter the number corresponding to your OS: " os_choice
 
-  # Set up the Docker repository based on OS selection
-  if [[ "$os_choice" == "1" ]]; then
-    print_info "Setting up Docker for Debian 11/12"
-    DOCKER_REPO="debian"
-    DOCKER_CODENAME="bullseye"
-  elif [[ "$os_choice" == "2" ]]; then
-    print_info "Setting up Docker for Ubuntu 20.04 (Focal)"
-    DOCKER_REPO="ubuntu"
-    DOCKER_CODENAME="focal"
-  elif [[ "$os_choice" == "3" ]]; then
-    print_info "Setting up Docker for Ubuntu 22.04 (Jammy)"
-    DOCKER_REPO="ubuntu"
-    DOCKER_CODENAME="jammy"
-  elif [[ "$os_choice" == "4" ]]; then
-    print_info "Setting up Docker for Ubuntu 18.04 (Bionic)"
-    DOCKER_REPO="ubuntu"
-    DOCKER_CODENAME="bionic"
-  elif [[ "$os_choice" == "5" ]]; then
-    print_info "Setting up Docker for Fedora"
-    OS_TYPE="fedora"
-  elif [[ "$os_choice" == "6" ]]; then
-    print_info "Setting up Docker for CentOS 7"
-    OS_TYPE="centos"
-  else
-    print_error "Invalid choice. Please run the script again and choose a valid option."
-    exit 1
-  fi
+  # Set up Docker repository and installation based on OS
+  case "$os_choice" in
+    1)
+      print_info "Setting up Docker for Debian 11/12"
+      DOCKER_REPO="debian"
+      DOCKER_CODENAME="bullseye"
+      ;;
+    2)
+      print_info "Setting up Docker for Ubuntu 20.04 (Focal)"
+      DOCKER_REPO="ubuntu"
+      DOCKER_CODENAME="focal"
+      ;;
+    3)
+      print_info "Setting up Docker for Ubuntu 22.04 (Jammy)"
+      DOCKER_REPO="ubuntu"
+      DOCKER_CODENAME="jammy"
+      ;;
+    4)
+      print_info "Setting up Docker for Ubuntu 18.04 (Bionic)"
+      DOCKER_REPO="ubuntu"
+      DOCKER_CODENAME="bionic"
+      ;;
+    5)
+      print_info "Setting up Docker for Fedora"
+      OS_TYPE="fedora"
+      ;;
+    6)
+      print_info "Setting up Docker for CentOS 7"
+      OS_TYPE="centos"
+      ;;
+    *)
+      print_error "Invalid choice. Please run the script again and choose a valid option."
+      exit 1
+      ;;
+  esac
 
-  # Docker installation steps based on OS type
+  # Docker installation steps for Debian/Ubuntu
   if [[ "$os_choice" -ge 1 && "$os_choice" -le 4 ]]; then
-    print_info "Installing Docker prerequisites for Debian/Ubuntu"
+    print_info "Installing Docker for Debian/Ubuntu"
     apt update
     apt install -y \
       ca-certificates \
@@ -145,7 +138,6 @@ if [[ "$install_docker" == "y" ]]; then
     print_info "Installing Docker"
     apt update
     apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
   elif [[ "$os_choice" == "5" ]]; then
     print_info "Installing Docker for Fedora"
     dnf install -y docker
@@ -154,19 +146,11 @@ if [[ "$install_docker" == "y" ]]; then
     yum install -y docker
   fi
 
-  # Start and enable Docker service
   print_info "Starting and enabling Docker service"
   systemctl start docker
   systemctl enable docker
 
-  # Add the new users to the docker group
-  for username in "${usernames[@]}"; do
-    print_info "Adding user '$username' to the docker group"
-    usermod -aG docker "$username"
-  done
-
-  # Ask if the user wants to install Docker Compose
-  read -p "Do you want to install Docker Compose? (y/n): " install_docker_compose
+  # Install Docker Compose if requested
   if [[ "$install_docker_compose" == "y" ]]; then
     print_info "Installing Docker Compose"
     DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -178,11 +162,13 @@ if [[ "$install_docker" == "y" ]]; then
     print_info "Skipping Docker Compose installation"
   fi
 
+  # Add the user to the Docker group
+  print_info "Adding user '$username' to the docker group"
+  usermod -aG docker "$username"
 else
   print_info "Skipping Docker installation"
 fi
 
 # Final message
 print_info "Setup Complete"
-echo -e "${GREEN}Users ${usernames[*]} have been created, added to sudo (if chosen), and Docker (if chosen) installed.${NC}"
 print_warning "It is recommended to reboot the system to ensure all changes take effect."
